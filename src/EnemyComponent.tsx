1d049ba04ec553a5e3d2279faa9b3b6263af199c
Shintaro Ishida <sh1sh1dax@gmail.com>
2018-11-10 18:22:56 +0900

    Display Enemy image

---
+++ src/EnemyComponent.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+export default class extends React.Component {
+
+  public render() {
+    return (
+      <div style={{ margin: 100 }}>
+        <img src="koboruto.png" />
+      </div>
+    )
+  }
+}
